89324c53782cf1d05a0bc1456e190c26e54e210b
Nigel Mpofu <nvmpofu@gmail.com>
2020-04-10 18:24:35 +0200

    Add worldometer table column count

--- server.js
+++ server.js
@@ -67,8 +67,10 @@ var getcountries = setInterval(async () => {
     .children("tr")
     .children("td");
 
-  // NOTE: this will change when table format change in website
-  const totalColumns = 12;
+  // count worldometers table columns
+  const colCount = html('table#main_table_countries_today th').length;
+
+  const totalColumns = colCount;
   const countryColIndex = 0;
   const casesColIndex = 1;
   const todayCasesColIndex = 2;
@@ -98,7 +100,7 @@ var getcountries = setInterval(async () => {
       country = country.trim();
       if (country.length === 0) {
         // parse with hyperlink
-        country = cell.children[0].next.children[0].data || "";
+        country = cell.children[0].next.children[0] && cell.children[0].next.children[0].data || "";
       }
       result.push({ country: country.trim() || "" });
     }
